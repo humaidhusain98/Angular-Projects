fe40b3e36fd0b16df5b5c06a308fc7515a8ae08d
humaidhusain98 <66073877+humaidhusain98@users.noreply.github.com>
2021-03-31 18:54:06 +0530

    Update environment.ts

--- MyFirstAngFirebaseApp/src/environments/environment.ts
+++ MyFirstAngFirebaseApp/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
    firebaseConfig:{
-    apiKey: "AIzaSyAgUsO4Mhp7l3vVJVPX_Evsf4fsjFgWeb0",
-    authDomain: "my-first-angular-firebas-29ce0.firebaseapp.com",
-    databaseURL: "https://my-first-angular-firebas-29ce0.firebaseio.com",
-    projectId: "my-first-angular-firebas-29ce0",
-    storageBucket: "my-first-angular-firebas-29ce0.appspot.com",
-    messagingSenderId: "471884153143",
-    appId: "1:471884153143:web:da531d64d22a795b9a9ab2",
-    measurementId: "G-FDYK44131E"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
